87f74a9246badafd54dc85bb84950c2c15c1ce7a
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-23 08:11:58 -0300

    initial repo

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully from Development!',
+        message: 'Go Serverless v2.0! Your function executed successfully from Authorizer!',
         input: event,
       },
       null,
