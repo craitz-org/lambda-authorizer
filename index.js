93429e1698d883a0c50846ffc431ae98736155a9
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-23 18:49:57 -0300

    form-urlencoded integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // layers
 const AWS = require('aws-sdk');
+const parser = require('query-string-parser')
 
 // AWS services
 const cognitoProviderLib = require('aws-sdk/clients/cognitoidentityserviceprovider');
@@ -21,20 +22,6 @@ function buildCustomError(httpStatus, cause) {
             cause
         });
       }
-      case 404: {
-          return JSON.stringify({
-              httpStatus,
-              type: 'NotFound',
-              cause
-          });
-      }
-      case 409: {
-          return JSON.stringify({
-              httpStatus,
-              type: 'DataIntegrityViolation',
-              cause
-          });
-      }
       case 500: {
           return JSON.stringify({
               httpStatus,
@@ -54,6 +41,7 @@ function buildCustomError(httpStatus, cause) {
 
 async function authorizeUser(clientId, poolId, username, password) {
     try {
+      // perform authorization on Cognito
         const data = await cognitoidentityserviceprovider.adminInitiateAuth({
             AuthFlow: 'ADMIN_NO_SRP_AUTH',
             ClientId: clientId,
@@ -67,12 +55,13 @@ async function authorizeUser(clientId, poolId, username, password) {
             throw buildCustomError(500, `Erro inesperado autenticando o usuário ${username} no Cognito`);
         }
 
-        return JSON.stringify({
+        // return access token
+        return {
             access_token: data.AuthenticationResult.IdToken,
             token_type: 'Bearer',
             expires_in: 3600,
             scope: 'Read'
-        });
+        };
     } catch (err) {
         if (err.code === 'NotAuthorizedException') {
             throw buildCustomError(401, `${err.code}: ${err.message}`);
@@ -84,10 +73,10 @@ async function authorizeUser(clientId, poolId, username, password) {
 
 exports.handler = async (event, context) => {
     try {
-        console.log({
-          event: JSON.stringify(event)
-        });
+        // parse x-www-form-urlencoded
+        event = parser.fromQuery(event['body-json'])
 
+        // authorization
         return await authorizeUser(event.clientId, event.poolId, event.username, event.password);
     } catch (err) {
         return err;
