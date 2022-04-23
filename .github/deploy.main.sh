7feb20067daa515eee516774b89201be179f4f11
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-23 08:40:28 -0300

    check alias

--- .github/deploy.main.sh
+++ .github/deploy.main.sh
@@ -13,6 +13,15 @@ echo "${result}"
 latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
 echo "Latest version: ${latest_version}"
 
-# update alias to published version
-result=$(aws lambda update-alias --function-name ${function_name} --name prod --function-version ${latest_version})
-echo "${result}"
+alias_name=$(aws lambda get-alias --function-name lambda-authorizer --name prod | grep "Name" | cut -d':' -f 2 | cut -d'"' -f 2)
+if [[ ${alias_name} ]]; then
+	echo "Updating alias..."
+	# update alias to published version
+	result=$(aws lambda update-alias --function-name ${function_name} --name prod --function-version ${latest_version})
+	echo "${result}"
+else
+	echo "Creating alias..."
+	# create alias to published version
+	result=$(aws lambda create-alias --function-name ${function_name} --name prod --function-version ${latest_version})
+	echo "${result}"
+fi
