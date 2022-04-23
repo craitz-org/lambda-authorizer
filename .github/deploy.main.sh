1f86804a714ebfe107f5d51e4c577c11d85ffbb2
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-23 08:55:09 -0300

    check alias

--- .github/deploy.main.sh
+++ .github/deploy.main.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -e
-
 # lambda name
 function_name="lambda-authorizer"
+alias_name="prod"
 
 # serverless deploy
 result=$(sls deploy --stage=prod)
@@ -13,15 +12,15 @@ echo "${result}"
 latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
 echo "Latest version: ${latest_version}"
 
-alias_name=$(aws lambda get-alias --function-name lambda-authorizer --name prod | grep "Name" | cut -d':' -f 2 | cut -d'"' -f 2)
-if [[ ${alias_name} ]]; then
+result_alias=$(aws lambda get-alias --function-name lambda-authorizer --name ${alias_name} | grep "Name" | cut -d':' -f 2 | cut -d'"' -f 2)
+if [[ ${result_alias} ]]; then
 	echo "Updating alias..."
 	# update alias to published version
-	result=$(aws lambda update-alias --function-name ${function_name} --name prod --function-version ${latest_version})
+	result=$(aws lambda update-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
 	echo "${result}"
 else
 	echo "Creating alias..."
 	# create alias to published version
-	result=$(aws lambda create-alias --function-name ${function_name} --name prod --function-version ${latest_version})
+	result=$(aws lambda create-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
 	echo "${result}"
 fi
