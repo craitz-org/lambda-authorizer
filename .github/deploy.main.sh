47de8bded4948b9b1443b6b9c0f7392cfd9d1d21
Camilo Raitz da Silva <craitzus@gmail.com>
2022-04-24 06:02:27 -0300

    multiple lambdas

--- .github/deploy.main.sh
+++ .github/deploy.main.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # lambda name
-function_name="lambda-authorizer-prd"
 alias_name="prd"
+function_name="lambda-authorizer-${alias_name}"
 
 # serverless deploy
 result=$(sls deploy --stage ${alias_name})
 echo "${result}"
 
-# find newest published version
-latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
-echo "Latest version: ${latest_version}"
+# # find newest published version
+# latest_version=$(aws lambda list-versions-by-function --function-name ${function_name} | grep "FunctionArn" | tail -1 | cut -d':' -f 9 | cut -d'"' -f 1)
+# echo "Latest version: ${latest_version}"
 
-result_alias=$(aws lambda get-alias --function-name lambda-authorizer --name ${alias_name} | grep "Name" | cut -d':' -f 2 | cut -d'"' -f 2)
-if [[ ${result_alias} ]]; then
-	echo "Updating alias..."
-	# update alias to published version
-	result=$(aws lambda update-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
-	echo "${result}"
-else
-	echo "Creating alias..."
-	# create alias to published version
-	result=$(aws lambda create-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
-	echo "${result}"
-fi
+# result_alias=$(aws lambda get-alias --function-name lambda-authorizer --name ${alias_name} | grep "Name" | cut -d':' -f 2 | cut -d'"' -f 2)
+# if [[ ${result_alias} ]]; then
+# 	echo "Updating alias..."
+# 	# update alias to published version
+# 	result=$(aws lambda update-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
+# 	echo "${result}"
+# else
+# 	echo "Creating alias..."
+# 	# create alias to published version
+# 	result=$(aws lambda create-alias --function-name ${function_name} --name ${alias_name} --function-version ${latest_version})
+# 	echo "${result}"
+# fi
